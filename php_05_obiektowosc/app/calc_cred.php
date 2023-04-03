e2c0aa27851f98c14399cec603b5eca86aae341b
ravduda <ravduda@gmail.com>
2023-04-03 19:04:38 +0200

    Added action router

--- php_05_obiektowosc/app/calc_cred.php
+++ php_05_obiektowosc/app/calc_cred.php
@@ -1,10 +1,20 @@
 <?php
 require_once dirname(__FILE__).'/../config.php';
 
-require_once $conf->root_path.'/app/CalcCredCtrl.class.php';
 
 $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
 
-$ctrl = new CalcCredCtrl();
-$ctrl->process();
+switch($action){
+	case 'calcCompute':
+		require_once $conf->root_path.'/app/CalcCredCtrl.class.php';
+		$ctrl = new CalcCredCtrl();
+		$ctrl->process();
+		break;
+	default:
+		require_once $conf->root_path.'/app/CalcCredCtrl.class.php';
+		$ctrl = new CalcCredCtrl();
+		$ctrl->process();
+		break;
+}
+
 
